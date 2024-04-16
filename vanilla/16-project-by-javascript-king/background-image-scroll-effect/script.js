2d3ea59046bc177a8ccbd7140acfd15213bb1b37
p4lm4d3v <p4lm4d3v@gmail.com>
2024-04-16 23:04:34 +0200

    v1.0.6
    finished:
        - background image scroll effect
    added:
        - drum kits
        - random photos

--- vanilla/16-project-by-javascript-king/background-image-scroll-effect/script.js
+++ vanilla/16-project-by-javascript-king/background-image-scroll-effect/script.js
@@ -8,4 +8,6 @@ function updateImage() {
   const val = Math.round((1 - window.pageYOffset / 800) * 100) / 100;
   bgImageEl.style.opacity = val <= 0.1 ? 0 : val;
   console.log(Math.round(window.pageYOffset * 100) / 100, val);
+  bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
 }
+
