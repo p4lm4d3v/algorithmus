e6a62a8d43b876dd269c0c4c2effc0c63de29170
p4lm4d3v <p4lm4d3v@gmail.com>
2024-04-21 13:34:03 +0200

    v1.0.8
    drum kits: tested the new cdnjs vscode extension to see if it works properly
    added cool-subscribe-button

--- vanilla/16-project-by-javascript-king/drum-kits/script.js
+++ vanilla/16-project-by-javascript-king/drum-kits/script.js
@@ -18,7 +18,7 @@ kits.forEach((kit) => {
   window.addEventListener("keydown", (e) => {
     if (e.key === kit.slice(0, 1)) {
       audioEl.play();
-      btnEl.style.transform = "scale(0.9)"; 
+      btnEl.style.transform = "scale(0.9)";
       setTimeout(() => {
         btnEl.style.transform = "scale(1)";
       }, 100);
---
+++ vanilla/cool-subscribe-button/README.md
@@ -0,0 +1,3 @@
+# Cool Subscribe Button
+
+- Made followin this [post](https://www.instagram.com/p/C53N74ZgaLX/?img_index=2) on Instagram
